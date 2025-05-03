e6453558ffc21128a63b401918cd3469611d9434
bhavikas-loca1 <bhavika.singh11@gmail.com>
2025-05-03 07:07:13 +0530

    removing unnecessary comments
    in temperature, temperature_dark and uv_index screens

--- weather_app/lib/models/weather_model.dart
+++ weather_app/lib/models/weather_model.dart
@@ -1,24 +1,3 @@
-// class Weather {
-//   final String cityName;
-//   final double temperature;
-//   final String mainCondition;
-  
-
-//   Weather({
-//     required this.cityName,
-//     required this.temperature,
-//     required this.mainCondition,
-
-//   });
-
-//   factory Weather.fromJson(Map<String, dynamic> json) {
-//     return Weather(
-//       cityName: json['name'],
-//       temperature: json['main']['temp'].toDouble(),
-//       mainCondition: json['weather'][0]['main'],
-//     );
-//   }
-// }
 class Weather {
   final String cityName;
   final double temperature;
